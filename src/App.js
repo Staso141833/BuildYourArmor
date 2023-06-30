4230de70859c2b529b06a04662f22c70b4d102d9
Stanislav-Stiptsov <stanislav.stiptsov@gmail.com>
2023-06-30 10:00:27 +0300

    Second

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import { Home } from './Components/Home/Home.jsx';
-import { Navbar } from './Components/Navbar/Navbar.jsx';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./Components/Home/Home.jsx";
+import { Navbar } from "./Components/Navbar/Navbar.jsx";
 
 function App() {
   return (
     <div className="App">
-        <Home></Home>
-        {/* <Navbar></Navbar> */}
+      <Routes>
+        <Route path="/home" element={<Home/>}/>;
+        <Route path="/nav" element={<Navbar/>}/>;
+      </Routes>
     </div>
   );
 }
