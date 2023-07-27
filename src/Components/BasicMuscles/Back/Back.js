285255156b0c2797959982ed86c1d30c687ccb07
Stanislav-Stiptsov <stanislav.stiptsov@gmail.com>
2023-07-27 12:02:34 +0300

    Back page has been updated

--- src/Components/BasicMuscles/Back/Back.js
+++ src/Components/BasicMuscles/Back/Back.js
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Card, CardMedia, Stack, Typography } from "@mui/material";
 
 export const Back = () => {
   return (
@@ -21,7 +21,7 @@ export const Back = () => {
           textAlign: "center",
           justifyContent: "space-evenly",
           width: "80%",
-          gap: 4,
+          gap: 6,
         }}
       >
         <Typography variant="h2">Back</Typography>
@@ -73,30 +73,39 @@ export const Back = () => {
               Main function: To keep the spine straight.
             </Typography>
           </Stack>
+
+          <Card position="relative" sx={{ width: "70%", mt: 2 }}>
+            <CardMedia
+              position="absolute"
+              component="img"
+              alt="Bill Pearl"
+              image="https://i.pinimg.com/originals/8b/f9/90/8bf990c510fcb0c0f9cb6b7e98c366c1.jpg"
+            ></CardMedia>
+          </Card>
         </Stack>
 
         <Stack
           sx={{
-            width: "70%",
+            width: "50%",
             display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-evenly",
+            flexDirection: "row",
+            justifyContent: "space-between",
             alignItems: "center",
             gap: 1,
           }}
         >
-          <Typography variant="h4">Upper back</Typography>
-          <Stack sx={{ width: "70%", border: "1px solid black" }}></Stack>
-
           <Stack
             sx={{
-              width: "70%",
+              width: "58%",
               display: "flex",
               flexDirection: "column",
               justifyContent: "space-evenly",
               gap: 2,
             }}
           >
+            <Typography variant="h4">Upper back</Typography>
+            <Stack sx={{ width: "100%", border: "1px solid black" }}></Stack>
+
             <Typography variant="p">
               Upper back development involves much more than the back muscles
               themselves. When you're in Double Biceps Back Pose, the trapezius
@@ -118,6 +127,29 @@ export const Back = () => {
               program for training the trapezius muscles.
             </Typography>
           </Stack>
+
+          <Stack
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "38%",
+              gap: 1,
+            }}
+          >
+            <Card position="relative" sx={{ width: "100%" }}>
+              <CardMedia
+                position="absolute"
+                component="img"
+                alt="Arnold"
+                image="https://www.greatestphysiques.com/wp-content/uploads/2016/09/1970-Arnold-1.jpg"
+              ></CardMedia>
+            </Card>
+            <Typography variant="p">
+              In the arched back pose, you need a solid, muscular upper back to
+              balance the development of the shoulders, biceps, triceps and
+              forearms.
+            </Typography>
+          </Stack>
         </Stack>
 
         <Stack
@@ -130,6 +162,14 @@ export const Back = () => {
             gap: 1,
           }}
         >
+          <Card position="relative" sx={{ width: "70%" }}>
+            <CardMedia
+              position="absolute"
+              component="img"
+              alt="Lee Haney"
+              image="https://i.skyrock.net/6142/43896142/pics/1769350702_small.jpg"
+            ></CardMedia>
+          </Card>
           <Typography variant="h4">Latissimus Dorsi</Typography>
           <Stack sx={{ width: "70%", border: "1px solid black" }}></Stack>
 
@@ -169,52 +209,97 @@ export const Back = () => {
               as well as the Double pose front and back biceps, and various bent
               over poses.
             </Typography>
+
+            <Stack
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
+                gap: 1,
+              }}
+            >
+              <Card position="relative" sx={{ width: "100%" }}>
+                <CardMedia
+                  position="absolute"
+                  component="img"
+                  alt="Ronnie Coleman"
+                  image="https://scontent.fsof10-1.fna.fbcdn.net/v/t1.6435-9/87260582_10158041932444621_7648843790714667008_n.jpg?stp=dst-jpg_p526x296&_nc_cat=104&cb=99be929b-3346023f&ccb=1-7&_nc_sid=730e14&_nc_ohc=aKg-1slaUE4AX9NOEQt&_nc_ht=scontent.fsof10-1.fna&oh=00_AfC2nx71aCVlb1zzPKoFu1YE8798KKMAGxmxWtD4DIUK6w&oe=64E9AD53"
+                ></CardMedia>
+              </Card>
+              <Typography variant="p">
+                Lee Haney, Ronnie Coleman and Robbie Robinson are three
+                incredible bodybuilders known for their V-shaped torsos in the
+                back and front, which is the result of the exceptional
+                development of the latissimus dorsi.
+              </Typography>
+            </Stack>
           </Stack>
         </Stack>
 
         <Stack
           sx={{
-            width: "70%",
+            width: "50%",
             display: "flex",
-            flexDirection: "column",
+            flexDirection: "row",
             justifyContent: "space-evenly",
             alignItems: "center",
             gap: 1,
           }}
         >
-          <Typography variant="h4">
-            Lower part of the latissimus dorsi
-          </Typography>
-          <Stack sx={{ width: "70%", border: "1px solid black" }}></Stack>
+          <Card position="relative" sx={{ width: "38%" }}>
+            <CardMedia
+              position="absolute"
+              component="img"
+              alt="Frank Zane"
+              image="https://i.pinimg.com/564x/c2/ba/76/c2ba76f61eeda7af08ccc1bff6f39c52.jpg"
+            ></CardMedia>
+          </Card>
 
           <Stack
             sx={{
-              width: "70%",
+              width: "58%",
               display: "flex",
               flexDirection: "column",
               justifyContent: "space-evenly",
-              gap: 2,
+              alignItems: "center",
+              gap: 1,
             }}
           >
-            <Typography variant="p">
-              When you see Franco Columbo or Frank Zane doing arched back pose,
-              you can't help but be impressed by the way the lower part of the
-              latissimus dorsi extend to their starting point at the waist. This
-              gives these muscles an incredibly aesthetic look.
+            <Typography variant="h4">
+              Lower part of the latissimus dorsi
             </Typography>
-            <Typography variant="p">
-              In order to develop the lower part of these muscles, you should
-              perform your back exercises with a lot of weight - Close Grip
-              Barbell Pulls and Close Grip Pull Ups for example, as well as One
-              Arm Pulley Pull Ups and One Arm Dumbbell Rows hand. In addition,
-              it is important to perform stretching exercises between sets so
-              that you feel a complete stretch in the lower part of the muscle.
-            </Typography>
-            <Typography variant="p">
-              A well-developed lower latissimus dorsi will also help you in back
-              poses, as they angle downward and form a sort of frame that
-              reveals a well-toned lower back.
-            </Typography>
+            <Stack sx={{ width: "100%", border: "1px solid black" }}></Stack>
+
+            <Stack
+              sx={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-evenly",
+                gap: 2,
+              }}
+            >
+              <Typography variant="p">
+                When you see Franco Columbo or Frank Zane doing arched back
+                pose, you can't help but be impressed by the way the lower part
+                of the latissimus dorsi extend to their starting point at the
+                waist. This gives these muscles an incredibly aesthetic look.
+              </Typography>
+              <Typography variant="p">
+                In order to develop the lower part of these muscles, you should
+                perform your back exercises with a lot of weight - Close Grip
+                Barbell Pulls and Close Grip Pull Ups for example, as well as
+                One Arm Pulley Pull Ups and One Arm Dumbbell Rows hand. In
+                addition, it is important to perform stretching exercises
+                between sets so that you feel a complete stretch in the lower
+                part of the muscle.
+              </Typography>
+              <Typography variant="p">
+                A well-developed lower latissimus dorsi will also help you in
+                back poses, as they angle downward and form a sort of frame that
+                reveals a well-toned lower back.
+              </Typography>
+            </Stack>
           </Stack>
         </Stack>
 
@@ -225,7 +310,7 @@ export const Back = () => {
             flexDirection: "column",
             justifyContent: "space-evenly",
             alignItems: "center",
-            gap: 1,
+            gap: 2,
           }}
         >
           <Typography variant="h4">Density in the middle back</Typography>
@@ -261,6 +346,29 @@ export const Back = () => {
               Barbell Rows.
             </Typography>
           </Stack>
+
+          <Stack
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "60%",
+              gap: 1,
+            }}
+          >
+            <Card position="relative">
+              <CardMedia
+                position="absolute"
+                component="img"
+                alt="Flex Wheeler"
+                image="https://preview.redd.it/one-of-bodybuildings-most-sculpted-backs-flex-wheeler-v0-s3qiygw5t8y91.png?width=640&crop=smart&auto=webp&s=20be455c76e5c8593281319b918da445f20bb94b"
+              ></CardMedia>
+            </Card>
+            <Typography variant="p">
+              The flex wheeler has the advantage due to an excellent genetic
+              predisposition: the density and musculature of his back shows how
+              hard he has trained to realize his potential.
+            </Typography>
+          </Stack>
         </Stack>
 
         <Stack
@@ -273,10 +381,7 @@ export const Back = () => {
             gap: 1,
           }}
         >
-          <Typography variant="h4">
-            Lower back (waist area, lower back)
-          </Typography>
-          <Stack sx={{ width: "70%", border: "1px solid black" }}></Stack>
+
 
           <Stack
             sx={{
@@ -287,6 +392,10 @@ export const Back = () => {
               gap: 2,
             }}
           >
+                      <Typography variant="h4">
+            Lower back (waist area, lower back)
+          </Typography>
+          <Stack sx={{ width: "70%", border: "1px solid black" }}></Stack>
             <Typography variant="p">
               Many of the best bodybuilders have a perfect upper back, but have
               never been able to develop the waist area to the extent that they
@@ -306,6 +415,29 @@ export const Back = () => {
               the waist area.
             </Typography>
           </Stack>
+
+          <Stack
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: "60%",
+              gap: 1,
+            }}
+          >
+            <Card position="relative">
+              <CardMedia
+                position="absolute"
+                component="img"
+                alt="Dorian Yates"
+                image="https://fitnessvolt.com/wp-content/uploads/2021/03/dorian-yates-christmas-tree-back.jpg"
+              ></CardMedia>
+            </Card>
+            <Typography variant="span">
+              Dorian Yates
+            </Typography>
+          </Stack>
+       
+        
         </Stack>
 
         <Stack
